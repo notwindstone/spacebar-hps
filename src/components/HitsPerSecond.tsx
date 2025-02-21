18d9c1d79d4cb39fc1fc5f03e9beb95567ccdc38
notwindstone <notwindstone@gmail.com>
2025-02-21 13:46:56 +0300

    feat: implement timer

--- src/components/HitsPerSecond.tsx
+++ src/components/HitsPerSecond.tsx
@@ -5,12 +5,28 @@ import { useEffect, useState } from "react";
 
 export default function HitsPerSecond() {
     const [hits, setHits] = useState(0);
+    const [started, setStarted] = useState(false);
+    const [milliseconds, setMilliseconds] = useState(0);
+
+    function handleTimeChange() {
+        setMilliseconds((ms) => ms + 100);
+    }
 
     function handleKeyPress(event: KeyboardEvent) {
         if (event.key === " ") {
+            if (!started) {
+                setStarted(true);
+            }
+
             setHits((count) => count + 1);
         }
     }
+
+    useEffect(() => {
+        if (started) {
+            setInterval(handleTimeChange, 100);
+        }
+    }, [started]);
 
     useEffect(() => {
         window.addEventListener("keyup", handleKeyPress);
@@ -22,9 +38,14 @@ export default function HitsPerSecond() {
 
     return (
         <div
-            className="text-white"
+            className="flex flex-col"
         >
-            {hits}
+            <p>
+                {hits}
+            </p>
+            <p>
+                {milliseconds}
+            </p>
         </div>
     );
 }
