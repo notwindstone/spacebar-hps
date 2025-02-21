f4ae3ff045687f827b3b1e17a2c6a96b6092b7b0
notwindstone <notwindstone@gmail.com>
2025-02-21 14:30:03 +0300

    feat: implement last five seconds counter

--- src/components/HitsPerSecond.tsx
+++ src/components/HitsPerSecond.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-
 import { useEffect, useState } from "react";
 
 export default function HitsPerSecond() {
+    const [hitsLastFiveSeconds, setHitsLastFiveSeconds] = useState(0);
     const [hits, setHits] = useState(0);
     const [milliseconds, setMilliseconds] = useState(0);
     const [started, setStarted] = useState(false);
     const [max, setMax] = useState(0);
 
     const seconds = milliseconds / 1000;
+    const lastFiveSeconds = Math.round((seconds % 5.0) * 10) / 10;
     const safeSeconds = seconds == 0 ? 1 : seconds;
+    const safeLastFiveSeconds = lastFiveSeconds == 0 ? 1 : lastFiveSeconds;
+
     const hps = Math.round(hits * 100 / safeSeconds) / 100;
+    const hpsLastFiveSeconds = Math.round(hitsLastFiveSeconds * 100 / safeLastFiveSeconds) / 100;
 
     function handleTimeChange() {
         setMilliseconds((ms) => ms + 100);
@@ -24,6 +28,7 @@ export default function HitsPerSecond() {
             }
 
             setHits((count) => count + 1);
+            setHitsLastFiveSeconds((count) => count + 1);
         }
     }
 
@@ -54,22 +59,41 @@ export default function HitsPerSecond() {
         };
     }, []);
 
+    useEffect(() => {
+        if (lastFiveSeconds === 0) {
+            setHitsLastFiveSeconds(0);
+        }
+    }, [seconds]);
+
     return (
         <div
             className="flex flex-col"
         >
             <p>
-                Total hits: {hits}
+                Total - hits: {hits}
             </p>
             <p>
-                Total time: {seconds}
+                Total - time: {seconds}
             </p>
             <p>
-                Total HPS: {hps}
+                Total - HPS: {hps}
             </p>
             <p>
-                Peak HPS: {max}
+                Total - Peak HPS: {max}
             </p>
+            <p>
+                Last 5 seconds - Hits: {hitsLastFiveSeconds}
+            </p>
+            <p>
+                Last 5 seconds - HPS: {hpsLastFiveSeconds}
+            </p>
+            <button
+                onClick={() => {
+                    window.location.reload();
+                }}
+            >
+                Reset
+            </button>
         </div>
     );
 }
