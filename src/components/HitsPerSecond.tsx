d9306af7f5531ac4cbcf35f8d6d225fa465aba82
notwindstone <notwindstone@gmail.com>
2025-02-21 14:09:06 +0300

    feat: implement better hits per second logic

--- src/components/HitsPerSecond.tsx
+++ src/components/HitsPerSecond.tsx
@@ -5,8 +5,13 @@ import { useEffect, useState } from "react";
 
 export default function HitsPerSecond() {
     const [hits, setHits] = useState(0);
-    const [started, setStarted] = useState(false);
     const [milliseconds, setMilliseconds] = useState(0);
+    const [started, setStarted] = useState(false);
+    const [max, setMax] = useState(0);
+
+    const seconds = milliseconds / 1000;
+    const safeSeconds = seconds == 0 ? 1 : seconds;
+    const hps = Math.round(hits * 100 / safeSeconds) / 100;
 
     function handleTimeChange() {
         setMilliseconds((ms) => ms + 100);
@@ -21,6 +26,19 @@ export default function HitsPerSecond() {
             setHits((count) => count + 1);
         }
     }
+
+    useEffect(() => {
+        if (milliseconds <= 1000) {
+            return;
+        }
+
+        setMax(
+            (currentMax) =>
+                Math.max(
+                    Math.round(currentMax * 100), Math.round(hps * 100)
+                ) / 100
+        );
+    }, [hits]);
 
     useEffect(() => {
         if (started) {
@@ -41,10 +59,16 @@ export default function HitsPerSecond() {
             className="flex flex-col"
         >
             <p>
-                {hits}
+                Total hits: {hits}
             </p>
             <p>
-                {milliseconds}
+                Total time: {seconds}
+            </p>
+            <p>
+                Total HPS: {hps}
+            </p>
+            <p>
+                Peak HPS: {max}
             </p>
         </div>
     );
